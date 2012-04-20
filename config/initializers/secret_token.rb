f6823055ddeb068e2c32ed194551185053f05b98
hyunukJung <moredream7@gmail.com>
2012-04-20 12:04:47 +0900

    Add twitter bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogRedis::Application.config.secret_token = '3568624ae8e88de74f1ac565e559ad649a0ce2a42dfea0ee2b80b5093996557bb8f97e7f82ebf78b9107780fb8353e8c3735ed8821cf786489b6f36dee854a29'
